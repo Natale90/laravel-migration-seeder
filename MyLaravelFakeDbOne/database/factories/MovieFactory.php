6b42013bfefb829fd06a035eb12ae41a82594789
Natale Capristo <andreacapristo1@gmail.com>
2021-05-24 16:51:40 +0200

    My First fake database

---
+++ MyLaravelFakeDbOne/database/factories/MovieFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Movie;
+use Faker\Generator as Faker;
+
+$factory->define(Movie::class, function (Faker $faker) {
+    return [
+
+        'title' => $faker -> word,
+        'original_title' => $faker -> word,
+        'nationality' => $faker -> state,
+        'date_of_release' => $faker -> dateTimeBetween($startDate = '-30 years', $endDate = 'now', $timezone = null),
+        'vote' => $faker -> numberBetween(5, 10),
+
+    ];
+});
